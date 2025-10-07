b5a420a30f85675183b7d63728d1aaebc3bc6c37
Aleksander Waligora <alekwaligorav2@gmail.com>
2025-10-07 12:42:09 +0200

    First commit

---
+++ helpers/visualisation_helpers.py
@@ -0,0 +1,32 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def draw_subplots(df,features_2darray,plot_type,x,y):
+    fig, axes = plt.subplots(x,y,figsize=(12,12))
+    for i in range(len(features_2darray)):
+        for j in range(len(features_2darray[i])):
+            if features_2darray[i][j] is not None:
+                col_name=features_2darray[i][j]
+                plot_type(data=df,x=col_name,ax=axes[i][j])
+                axes[i][j].set_title(col_name)
+                axes[i][j].set_xticklabels(labels=axes[i][j].get_xticklabels(),rotation=60)
+                
+            else:
+                fig.delaxes(axes[i][j])
+    fig.tight_layout()
+
+
+def feature_importance_plot(importance_list,features,model_name):
+    feature_importance=[[feature,abs(importance)] 
+                        for (feature,importance) in 
+                        zip(list(features),importance_list)]
+    feature_importance.sort(key = lambda x : x[1],reverse=True)
+    
+    df_fi = pd.DataFrame(feature_importance)
+    fig = sns.barplot(df_fi,x=0,y=1,)
+    fig.set_xticklabels(rotation=90,labels=df_fi[0])
+    fig.set_ylabel(None)
+    fig.set_xlabel(None)
+    fig.set_title(f"{model_name} feature importance")
+    plt.tight_layout()
